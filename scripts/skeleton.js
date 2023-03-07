a38d069b90c1fc2157cd78d6ec2976e68c46802a
Jeen Namkung <jnamkung@my.bcit.ca>
2023-03-06 18:22:21 -0800

    simple changes

--- scripts/skeleton.js
+++ scripts/skeleton.js
@@ -3,7 +3,8 @@
 // (navbar, footer, and other things) into html doc. 
 //---------------------------------------------------
 function loadSkeleton(){
-    console.log($('#navbarPlaceholder').load('./text/nav.html'));
+    console.log($('#navbarPlaceholder').load('./text/loggedin_nav.html'));
+    console.log($('#navbarPlaceholder').load('./text/unloggedin_nav.html'));
     console.log($('#footerPlaceholder').load('./text/footer.html'));
 }
 loadSkeleton();  //invoke the function
