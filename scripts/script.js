b22483faff84925e8e2e1e7793c3fb2df9d55b4f
Jeen Namkung <jnamkung@my.bcit.ca>
2023-03-08 09:39:20 -0800

    pull name and location from database

--- scripts/script.js
+++ scripts/script.js
@@ -23,26 +23,26 @@ insertName(); //run the function
 var userProfileEdit = document.querySelector('#editBtn-UserProfile');
 userProfileEdit.addEventListener("click", flipEditForm);
 
-function flipEditForm(){
+function flipEditForm() {
     let hiddenForm = document.getElementById('hiddenForm');
     hiddenForm.style.display = "block";
 
     const imageInput = document.getElementById('imageFile');
     const userImageElement = document.getElementById('userprofile-img');
 
-imageInput.addEventListener("change", function() {
-    const file = imageInput.files[0];
-    const reader = new FileReader();
+    imageInput.addEventListener("change", function () {
+        const file = imageInput.files[0];
+        const reader = new FileReader();
 
-    reader.addEventListener("load", function(){
-        userImageElement.src = reader.result;
+        reader.addEventListener("load", function () {
+            userImageElement.src = reader.result;
+        })
+
+        reader.readAsDataURL(file);
     })
-
-    reader.readAsDataURL(file);
-})
 }
 
-function confirmChanges_Clicked (){
+function confirmChanges_Clicked() {
     let userNameChange = document.getElementById('myName').value;
     let userLocationChange = document.getElementById('myLocation').value;
 
@@ -56,7 +56,7 @@ function confirmChanges_Clicked (){
 
 }
 
-var confirmChangesBtn =  document.getElementById('confirmChangesBtn');
+var confirmChangesBtn = document.getElementById('confirmChangesBtn');
 confirmChangesBtn.addEventListener('click', confirmChanges_Clicked);
 
 
@@ -64,11 +64,11 @@ confirmChangesBtn.addEventListener('click', confirmChanges_Clicked);
 
 // This section deals with posting on the main page
 
-function submitPost(){
+function submitPost() {
     alert("SUBMIT POST METHOD HAS BEEN TRIGGERED");
     //checks if user is logged in
-    firebase.auth().onAuthStateChanged(function(user){
-        if(user){
+    firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {
             //grabbing text inputs
             let titletxt = document.getElementById("title").value;
             let locationtxt = document.getElementById("location").value;
@@ -81,7 +81,7 @@ function submitPost(){
                 title: titletxt,
                 time_posted: firebase.firestore.FieldValue.serverTimestamp()
                 //then runs the upload pic javascript
-            }).then(doc =>{
+            }).then(doc => {
                 console.log("Post uploaded");
                 console.log(doc.id);
                 uploadPic(doc.id);
@@ -90,16 +90,16 @@ function submitPost(){
             console.log("ERROR USER NOT LOGGED IN")
         }
     })
-    
+
 }
 //universal image file reference
 let imageFile;
 //checks if in image has been chosen and displays it
-function listenFileSelect(){
+function listenFileSelect() {
     let fileInput = document.getElementById("mypic-input");
     const image = document.getElementById("mypic-goes-here");
 
-    fileInput.addEventListener('change', function(e){
+    fileInput.addEventListener('change', function (e) {
         imageFile = e.target.files[0];
         let blob = URL.createObjectURL(imageFile);
         image.src = blob;
@@ -107,65 +107,73 @@ function listenFileSelect(){
 }
 
 //uploads photo to fire base
-function uploadPic(postDocID){
+function uploadPic(postDocID) {
     console.log("inside uploadPic" + postDocID);
     //stores image in firebase storage as the postdocid.jpg
     let storageRef = storage.ref("images/" + postDocID + ".jpg");
-    
+
     storageRef.put(imageFile)
-    .then(function(){
-        console.log("Uploaded to CLoud Storage");
-        storageRef.getDownloadURL()
-        .then(function(url){
-            console.log("got th download URL");
-            db.collection("posts").doc(postDocID).update({
-                "image": url
-            })
-            .then(function(){
-                console.log("added pic URL to firebase")
-            })
+        .then(function () {
+            console.log("Uploaded to CLoud Storage");
+            storageRef.getDownloadURL()
+                .then(function (url) {
+                    console.log("got th download URL");
+                    db.collection("posts").doc(postDocID).update({
+                        "image": url
+                    })
+                        .then(function () {
+                            console.log("added pic URL to firebase")
+                        })
+                })
         })
-    })
-    .catch((error) => {
-        console.log("Error uploading to cloud server")
-    })
+        .catch((error) => {
+            console.log("Error uploading to cloud server")
+        })
 }
+
 function displayCardsDynamically(collection) {
     let cardTemplate = document.getElementById("postTemplate");
 
-    db.collection(collection).get()   //the collection called "hikes"
+    db.collection(collection).get()
         .then(allPosts => {
-            //var i = 1;  //Optional: if you want to have a unique ID for each hike
-            allPosts.forEach(doc => { //iterate thru each doc
-                var title = doc.data().title;       // get value of the "name" key
-                var description = doc.data().description;  // get value of the "details" key
-								var location = doc.data().location;    //get unique ID to each hike to be used for fetching right image
-                var time = doc.data().time_posted; //gets firebase time stamp
-                var docID = doc.id; //gets doc id
-                var owner = doc.data().owner; //gets user.uid
-                let image = doc.data().image; // gets image url
-                let newcard = cardTemplate.content.cloneNode(true); // references and clones card template
-                let date = new Date(time.seconds*1000); // formats time stamp into a date and time
-                //update title and text and image
+            allPosts.forEach(doc => {
+                var title = doc.data().title;
+                var description = doc.data().description;
+                var location = doc.data().location;
+                var time = doc.data().time_posted;
+                var docID = doc.id;
+                var owner = doc.data().owner;
+                let image = doc.data().image;
+                let newcard = cardTemplate.content.cloneNode(true);
+                let date = new Date(time.seconds * 1000);
+
                 newcard.querySelector('.card-title').innerHTML = title;
                 newcard.querySelector('.card-length').innerHTML = date;
                 newcard.querySelector('.card-text').innerHTML = description;
                 newcard.querySelector('.card-image').src = image;
-                // newcard.querySelector('.card-image').src = `./images/${hikeCode}.jpg`; //Example: NV01.jpg
-                // newcard.querySelector('a').href = "eachHike.html?docID="+docID;
 
-                //Optional: give unique ids to all elements for future use
-                // newcard.querySelector('.card-title').setAttribute("id", "ctitle" + i);
-                // newcard.querySelector('.card-text').setAttribute("id", "ctext" + i);
-                // newcard.querySelector('.card-image').setAttribute("id", "cimage" + i);
-
-                //attach to gallery, Example: "hikes-go-here"
                 document.getElementById(collection + "-go-here").appendChild(newcard);
-
-                //i++;   //Optional: iterate variable to serve as unique ID
-                
             })
         })
-        
+
 }
 displayCardsDynamically("posts");
+
+function displayCardsDynamically(collection) {
+    let cardTemplate = document.getElementById("userprofile");
+
+    db.collection(collection).get()
+        .then(allUsers=> {
+            allUsers.forEach(doc => {
+                var name = doc.data().name;
+                var location = doc.data().location;
+                let newcard = cardTemplate.content.cloneNode(true);
+
+                newcard.querySelector('#userName').innerHTML = name;
+                newcard.querySelector('#userLocation').innerHTML = location;
+                document.getElementById(collection + "-go-here").appendChild(newcard);
+            })
+        })
+}
+
+displayCardsDynamically("users");
