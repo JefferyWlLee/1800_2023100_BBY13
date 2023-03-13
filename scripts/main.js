014226b6754939272dc9fa16c9b4a18b2c62c82a
Jeff <jefferywllee@gmail.com>
2023-03-13 10:28:25 -0700

    added universal script to template.html and started post details page in each_post.html and eachpost_.js

--- scripts/main.js
+++ scripts/main.js
@@ -26,8 +26,7 @@ function displayCardsDynamically(collection) {
                     newcard.querySelector('.card-length').innerHTML = date;
                     newcard.querySelector('.card-text').innerHTML = userName;
                     newcard.querySelector('.card-image').src = image;
-                    // newcard.querySelector('.card-image').src = `./images/${hikeCode}.jpg`; //Example: NV01.jpg
-                    // newcard.querySelector('a').href = "eachHike.html?docID="+docID;
+                    newcard.querySelector('a').href = "each_post.html?docID="+docID;
 
                     //Optional: give unique ids to all elements for future use
                     // newcard.querySelector('.card-title').setAttribute("id", "ctitle" + i);
