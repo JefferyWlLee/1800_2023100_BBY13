af1e53a425316a5fe0924fa571efc478f539848a
Yousuf Rabani <mrabani@my.bcit.ca>
2023-03-20 20:42:53 -0700

    Remove redundant lines

--- scripts/script.js
+++ scripts/script.js
@@ -19,7 +19,7 @@ const thumb_Down = document.getElementById("thumbDown");
 
 
 /* This loops through each document in the users collection, checking if verified field exists, if
-   it exists then do nothing if it doesn't create field and set it to false.
+   it exists then does nothing if it doesn't creates a field and sets it to false.
    We use it once to add the fields to every user document and afterwards we comment it out in case we need it
    in the future.  
    */
@@ -27,9 +27,7 @@ const thumb_Down = document.getElementById("thumbDown");
 // usersCollection.get().then(querySnapshot => {
 //     querySnapshot.forEach(doc => {
 //       const data = doc.data();
-//       // Check if the Verified field exists in the current document
 //       if (!data.hasOwnProperty("Verified")) {
-//         // If the Verified field doesn't exist, add it with a default value of false
 //         doc.ref.set({ Verified: false }, { merge: true })
 //           .then(() => {
 //             console.log(`Added Verified field to document ${doc.id}`);
