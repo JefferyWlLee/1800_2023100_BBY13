d659c201bd5f4f211c12e689ec47f6e96dbf52c0
Yousuf Rabani <mrabani@my.bcit.ca>
2023-03-20 18:46:53 -0700

    Add code that loops through each document in the users collection, checking if verified field exists, if    it exists then do nothing if it doesn't create field and set it to false

---
+++ scripts/script.js
@@ -0,0 +1,43 @@
+// Code for reviewing/rating other users
+
+let thumb_Up = document.getElementById("thumbUp");
+let thumb_Down = document.getElementById("thumbDown");
+
+// thumb_Up.addEventListener("click", ()=> {
+    
+// });
+
+// function tup_clicked(){
+//    if user
+// }
+
+// function calculate_Rating() {
+//     if tup_clicked()
+// }
+
+// Get a reference to the users collection in Firestore
+const usersCollection = firebase.firestore().collection("users");
+
+
+
+/* This loops through each document in the users collection, checking if verified field exists, if
+   it exists then do nothing if it doesn't create field and set it to false */ 
+usersCollection.get().then(querySnapshot => {
+    querySnapshot.forEach(doc => {
+      const data = doc.data();
+      // Check if the Verified field exists in the current document
+      if (!data.hasOwnProperty("Verified")) {
+        // If the Verified field doesn't exist, add it with a default value of false
+        doc.ref.set({ Verified: false }, { merge: true })
+          .then(() => {
+            console.log(`Added Verified field to document ${doc.id}`);
+          })
+          .catch(error => {
+            console.error(`Error adding Verified field to document ${doc.id}: ${error}`);
+          });
+      }
+    });
+  })
+  .catch(error => {
+    console.error("Error retrieving user documents:", error);
+  });
