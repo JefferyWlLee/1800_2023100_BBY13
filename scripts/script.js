8ad4fecac2609112319214c9f2d8a3ec070760f4
Yousuf Rabani <mrabani@my.bcit.ca>
2023-03-20 20:41:27 -0700

    Comment out the code for creating verified fields in user documents in firestore database

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,8 @@
 // Code for reviewing/rating other users
 
-let thumb_Up = document.getElementById("thumbUp");
-let thumb_Down = document.getElementById("thumbDown");
+const usersCollection = firebase.firestore().collection("users");
+const thumb_Up = document.getElementById("thumbUp");
+const thumb_Down = document.getElementById("thumbDown");
 
 // thumb_Up.addEventListener("click", ()=> {
     
@@ -15,29 +16,30 @@ let thumb_Down = document.getElementById("thumbDown");
 //     if tup_clicked()
 // }
 
-// Get a reference to the users collection in Firestore
-const usersCollection = firebase.firestore().collection("users");
-
 
 
 /* This loops through each document in the users collection, checking if verified field exists, if
-   it exists then do nothing if it doesn't create field and set it to false */ 
-usersCollection.get().then(querySnapshot => {
-    querySnapshot.forEach(doc => {
-      const data = doc.data();
-      // Check if the Verified field exists in the current document
-      if (!data.hasOwnProperty("Verified")) {
-        // If the Verified field doesn't exist, add it with a default value of false
-        doc.ref.set({ Verified: false }, { merge: true })
-          .then(() => {
-            console.log(`Added Verified field to document ${doc.id}`);
-          })
-          .catch(error => {
-            console.error(`Error adding Verified field to document ${doc.id}: ${error}`);
-          });
-      }
-    });
-  })
-  .catch(error => {
-    console.error("Error retrieving user documents:", error);
-  });
+   it exists then do nothing if it doesn't create field and set it to false.
+   We use it once to add the fields to every user document and afterwards we comment it out in case we need it
+   in the future.  
+   */
+
+// usersCollection.get().then(querySnapshot => {
+//     querySnapshot.forEach(doc => {
+//       const data = doc.data();
+//       // Check if the Verified field exists in the current document
+//       if (!data.hasOwnProperty("Verified")) {
+//         // If the Verified field doesn't exist, add it with a default value of false
+//         doc.ref.set({ Verified: false }, { merge: true })
+//           .then(() => {
+//             console.log(`Added Verified field to document ${doc.id}`);
+//           })
+//           .catch(error => {
+//             console.error(`Error adding Verified field to document ${doc.id}: ${error}`);
+//           });
+//       }
+//     });
+//   })
+//   .catch(error => {
+//     console.error("Error retrieving user documents:", error);
+//   });
