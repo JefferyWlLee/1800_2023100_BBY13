5201b764c435c24b01d24f6283b8deb2067b7d04
Jeff <jefferywllee@gmail.com>
2023-03-25 09:52:58 -0700

    added tags to post cards on main page

--- scripts/main.js
+++ scripts/main.js
@@ -10,12 +10,15 @@ function displayCardsDynamically(collection) {
                 var docID = doc.id; //gets doc id
                 var owner = doc.data().owner; //gets user.uid
                 let image = doc.data().image; // gets image url
+                let tags = doc.data().tag //gets the tags associated with the post
+                tags = tags.toString();
                 let newcard = cardTemplate.content.cloneNode(true); // references and clones card template
                 var helping = doc.data().helping; //is the poster looking for help or giving help?
                 newcard.querySelector('.card-title').innerHTML = title;
                 newcard.querySelector('.card-length').innerHTML = time;
                 newcard.querySelector('.card-image').src = image;
                 newcard.querySelector('.card-help').innerHTML = helping;
+                newcard.querySelector('.tags-go-here').innerHTML = "Tags: " + tags;
                 // Changed querySelector paramter from 'a' to '.card-Button' - Yousuf '.card-text'
                 newcard.querySelector('.card-Button').href = "each_post.html?docID="+docID;
                 db.collection("users").doc(owner).get().then(userDoc => {
@@ -88,5 +91,60 @@ function saveUserNameToLocalStorage(event) {
     // Slice it to only target the parts AFTER "Posted by " and save it to userName variable
     const userName = postText.slice(userNameStartIndex);
     localStorage.setItem('userName', userName);
+  }
+
+  function filter(keyword){
+    document.getElementById("posts-go-here").innerHTML = "";
+
+    db.collection("posts").where("tags", "==" , keyword)   
+        .then(allPosts => {
+            //var i = 1;  //Optional: if you want to have a unique ID for each post
+            allPosts.forEach(doc => { //iterate thru each doc
+                var title = doc.data().title;       // get value of the "name" key
+                var time = doc.data().time_posted.toDate().toLocaleDateString(); //gets firebase time stamp
+                var docID = doc.id; //gets doc id
+                var owner = doc.data().owner; //gets user.uid
+                let image = doc.data().image; // gets image url
+                let newcard = cardTemplate.content.cloneNode(true); // references and clones card template
+                var helping = doc.data().helping; //is the poster looking for help or giving help?
+                newcard.querySelector('.card-title').innerHTML = title;
+                newcard.querySelector('.card-length').innerHTML = time;
+                newcard.querySelector('.card-image').src = image;
+                newcard.querySelector('.card-help').innerHTML = helping;
+                // Changed querySelector paramter from 'a' to '.card-Button' - Yousuf '.card-text'
+                newcard.querySelector('.card-Button').href = "each_post.html?docID="+docID;
+                db.collection("users").doc(owner).get().then(userDoc => {
+                    //get username of whoever made the post 
+                    var userName = userDoc.data().name;
+                    let currentUser = firebase.auth().currentUser;
+
+                    let nameElement = newcard.querySelector('.card-text');
+                    let nameLinkElement = document.createElement('a');
+                    nameLinkElement.innerHTML = "Posted By: " + userName;
+
+                    //Check if the current user is the owner of the post
+                    if (owner === currentUser.uid) {
+                        nameLinkElement.href = "my_user_profile.html";
+                    } else {
+                        nameLinkElement.href = "other_userProfile.html?userId=" + owner;
+                    }
+
+                    nameElement.innerHTML = "";
+                    nameElement.appendChild(nameLinkElement);
+                    
+                    // newcard.querySelector('.card-text').innerHTML = "Posted By: " + userName;
+
+                    /* Note to Jeff: This line adds a click event listeenr to each p element created which calls 
+                       saveUserNameToLocalStorage function when clicked - Yousuf */  
+                    newcard.querySelector('.card-text').addEventListener("click",  (event)=> {
+                        saveUserNameToLocalStorage(event);
+                    });
+                    
+
+                    //attach to gallery, Example: "hikes-go-here"
+                    document.getElementById(collection + "-go-here").appendChild(newcard);
+                })              
+            })
+        })    
   }
 
