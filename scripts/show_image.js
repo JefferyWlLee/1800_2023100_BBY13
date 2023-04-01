1e485097afc959187ad1e0a54028afd260400949
Yousuf Rabani <mrabani@my.bcit.ca>
2023-03-31 17:08:12 -0700

    Create file for showing user profile with function showUserPicture() which displays users profile pic on the nav bar

---
+++ scripts/show_image.js
@@ -0,0 +1,25 @@
+
+function showUserPicture() {
+  // Get the user's profile picture URL
+  firebase.auth().onAuthStateChanged(user => {
+    if (user) {
+        const currentUserRef = db.collection("users").doc(user.uid);
+        currentUserRef.get().then(userDoc => {
+            
+            const userProfileImg = userDoc.data().profileimg;
+           
+            const image = document.getElementById("user-profile-picture");
+            if (image) {
+                image.src = userProfileImg;
+            } else {
+                console.error("Image element not found");
+            }
+        });
+    } else {
+        console.log("No user is signed in");
+    }
+});
+}
+window.onload = showUserPicture;
+
+//done by yousuf
